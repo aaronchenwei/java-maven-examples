ad5137310cc472a9d1195b603b6140e6e7d27ce7
aaronchenwei <aaronchenwei@hotmail.com>
2022-08-03 09:43:20 +0800

    Project init

---
+++ jmh/jmh-toy-maven/src/main/java/org/toy/java/jmh/Code.java
@@ -0,0 +1,11 @@
+package org.toy.java.jmh;
+
+public class Code {
+    public static int method1(int a, int b) {
+        return a + b;
+    }
+
+    public static int method2(int a, int b) {
+        return a * b;
+    }
+}
