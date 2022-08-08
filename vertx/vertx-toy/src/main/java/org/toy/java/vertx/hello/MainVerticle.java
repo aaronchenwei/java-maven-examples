65316705342182b69912b5fe0e325018a6fbb8a8
aaronchenwei <aaronchenwei@hotmail.com>
2022-08-08 20:00:09 +0800

    Create project vertx-toy

---
+++ vertx/vertx-toy/src/main/java/org/toy/java/vertx/hello/MainVerticle.java
@@ -0,0 +1,31 @@
+package org.toy.java.vertx.hello;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Launcher;
+import io.vertx.core.Promise;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainVerticle extends AbstractVerticle {
+
+    public static void main(final String[] args) {
+        Launcher.executeCommand("run", MainVerticle.class.getName());
+    }
+
+    @Override
+    public void start(Promise<Void> startPromise) {
+        vertx.createHttpServer()
+            .requestHandler(
+                req -> req.response()
+                    .putHeader("content-type", "text/plain")
+                    .end("Hello from Vert.x!"))
+            .listen(8888, http -> {
+                if (http.succeeded()) {
+                    startPromise.complete();
+                    log.info("HTTP server started on port 8888");
+                } else {
+                    startPromise.fail(http.cause());
+                }
+            });
+    }
+}
